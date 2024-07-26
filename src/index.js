a14ae61adb183128fb17ea4ffb921260217e7bc2
Icaro Silva <dsicejay@gmail.com>
2024-07-26 14:56:38 -0300

    Realizando deploy do projeto

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import prompt from "prompt";
+import promptSchemaMain from "./prompts-schema/prompt-schema-main.js";
+import createQRCode from "./services/qr-code/create.js";
+import createPassword from "./services/password/create.js";
+
+async function main(){
+    prompt.get(promptSchemaMain, async (err, choice) => {
+
+        if(err) console.log(err)
+    
+
+        if(choice.select == 1) await createQRCode();
+        if(choice.select == 2) await createPassword();
+    })
+}
+
+main();
